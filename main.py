801e23b367349dd5da415fb475f685f5b63d82ed
Juan Di Pasquo <juandipasquo@gmail.com>
2021-11-25 20:31:58 -0300

    Request and Response Body

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+# Python
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# FastAPI
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+# Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 @app.get('/')
 def home():
     return {'Hello': 'World'}
+
+# Request and Response Body
+@app.post('/person/new')
+def create_person(person: Person = Body(...)):#* Los "..." significan que el parametro es OBLIGATORIO
+    return person
