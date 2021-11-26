c4699753e545301633471f5aac4acaea67e23747
Juan Di Pasquo <juandipasquo@gmail.com>
2021-11-25 21:17:46 -0300

    validations path

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 
 # FastAPI
 from fastapi import FastAPI
-from fastapi import Body, Query
+from fastapi import Body, Query, Path
 
 app = FastAPI()
 
@@ -31,7 +31,30 @@ def create_person(person: Person = Body(...)):#* Los "..." significan que el par
 
 @app.get('/person/detail')
 def show_person(
-    name: Optional[str] = Query(None, min_length=1, max_length=50),
-    age: int = Query(...)
+    name: Optional[str] = Query(
+        None,
+        min_length=1,
+        max_length=50,
+        title='Person Name',
+        description="This is the person name. It's between 1 and 50 characters"
+        ),
+    age: int = Query(
+        ...,
+        title='Person Age',
+        description="This is the person age. It's require"
+        )
 ):
     return {name: age}
+
+# Validaciones: Path Parameters
+
+@app.get('/person/detail/{person_id}')
+def show_person(
+    person_id: int = Path(
+        ...,
+        gt=0,
+        title='Person ID',
+        description='This is the person ID'
+        )
+):
+    return {person_id: 'It exists!'}
